cb4914d85697fc4168d5fe0650154cf785d0b1c6
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2022-01-10 02:33:12 +0200

    added icons

--- src/components/MainPage/Footer/Contacts/Contacts.js
+++ src/components/MainPage/Footer/Contacts/Contacts.js
@@ -5,9 +5,22 @@ const Contacts = () => {
   return (
     <div className={style.container}>
       <p>Контакты</p>
-      <p> 100000, Руспублика Казахстан, г.Караганда, ул. Телевизионная 10</p>
-      <p>&#9990; +38(000) 000 00 00 </p>
-      <p>&#128386; main@gmail.com</p>
+      <p>
+        <img
+          className={style.iconContact}
+          src="https://img.icons8.com/ios/32/000000/point-of-interest.png"
+          alt=""
+        />
+        &nbsp; 100000, Руспублика Казахстан, г.Караганда, ул. Телевизионная 10
+      </p>
+      <p>
+        <img src="https://img.icons8.com/small/32/000000/phone.png" alt="" />
+        &nbsp; +38(000) 000 00 00{" "}
+      </p>
+      <p>
+        <img src="https://img.icons8.com/small/32/000000/new-post.png" alt="" />
+        &nbsp; main@gmail.com
+      </p>
     </div>
   );
 };
